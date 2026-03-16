28f8b22f41bbb9b8f8b00756cf37cc627d806f77
Joy Patel <pateljoy149@gmail.com>
2026-03-15 20:25:46 -0400

    DDD Fix - Updated DbContext for vehicle and inventory both tables

--- VehicleInventory/VehicleInventory.Infrastructure/Data/JPInventoryDbContext.cs
+++ VehicleInventory/VehicleInventory.Infrastructure/Data/JPInventoryDbContext.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using VehicleInventory.Domain.Aggregates.JPVehicle;
+using VehicleInventory.Domain.ValueObjects;
 
 namespace VehicleInventory.Infrastructure.Data
 {
@@ -15,6 +16,48 @@ namespace VehicleInventory.Infrastructure.Data
         {}  
 
         public DbSet<JPVehicle> Vehicles { get; set; }
+        public DbSet<JPVehicleInventory> VehicleInventories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JPVehicle>(entity =>
+            {
+
+                entity.HasKey(v => v.Id);
+
+                entity.Property(v => v.VehicleCode)
+                    .HasConversion(v => v.Code, v => new JPVehicleCode(v))
+                    .IsRequired();
+
+                entity.Property(v => v.LocationId)
+                    .HasConversion(v => v.Location, v => new JPLocationId(v))
+                    .IsRequired();
+
+                entity.Property(v => v.VehicleType)
+                    .HasConversion(v => v.TypeName, v => new JPVehicleTypeId(v))
+                    .IsRequired();
+
+                entity.HasMany(v => v.InventoryRecords)
+                    .WithOne()
+                    .HasForeignKey(i => i.VehicleId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<JPVehicleInventory>(entity =>
+            {
+
+                entity.HasKey(i => i.Id);
+
+                entity.Property(i => i.Location)
+                    .HasConversion(v => v.Location, v => new JPLocationId(v))
+                    .IsRequired();
+
+
+                entity.Property(i => i.Quantity).IsRequired();
+
+                entity.Property(i => i.LastUpdated).IsRequired();
+            });
+        }
 
     }
 }
